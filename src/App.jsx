3095148b66b67241e464d3087368d2e78fc72c3e
leeosilvp <leo965679@gmail.com>
2025-09-07 18:56:23 -0300

    5 a- criando apresentação da interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './css/style.css'
 import Header from './components/Header'
-import Apresentation from './components/Apresentation'
+import Presentation from './components/Presentation'
 import ChangeIcon from './hooks/ChangeIcon'
 import Icon from './components/Icon'
+import imgInterface from './assets/img/interface.png'
 
 function App() {
 
@@ -11,9 +12,33 @@ function App() {
   return (
     <>
       <Header />
-      <div className="content">
-        <Apresentation />
-      </div>
+      <section className="content">
+        <Presentation />
+        <section className="interface">
+          <h1>Completely intuitive interface</h1>
+          <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <img src={imgInterface} alt="interface" />
+          <div className="content-interface">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit dolor sit amet consectetur.</p>
+            <section className="ctn-cards-interface">
+              <article className="card-interface">
+                <p>😊</p>
+                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit dolor sit amet consectetur.</h3>
+              </article>  
+
+              <article className="card-interface">
+                <p>😊</p>
+                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit dolor sit amet consectetur.</h3>
+              </article>
+
+              <article className="card-interface">
+                <p>😊</p>
+                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit dolor sit amet consectetur.</h3>
+              </article>
+            </section>
+          </div>
+        </section>
+      </section>
       <Icon />
     </>
   )
---
+++ src/components/Presentation.jsx
@@ -0,0 +1,15 @@
+import '../css/presentation.css'
+
+function Presentation() {
+    return (
+        <section>
+            <div className="presentation">
+                <h1>Anonymous AI</h1>
+                <h2>First GPT-5 powered interface</h2>
+                <button>Learn more</button>
+            </div>
+        </section>
+    )
+}
+
+export default Presentation

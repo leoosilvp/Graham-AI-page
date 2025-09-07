bc1d7ce970f40ba49251b33b93d19499ace863cf
leeosilvp <leo965679@gmail.com>
2025-09-07 18:04:30 -0300

    4 a- criando hook d trocar de icon conforme o tema do navegador

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './css/style.css'
 import Header from './components/Header'
 import Apresentation from './components/Apresentation'
+import ChangeIcon from './hooks/ChangeIcon'
 import Icon from './components/Icon'
 
 function App() {
+
+  ChangeIcon();
+
   return (
     <>
       <Header />
